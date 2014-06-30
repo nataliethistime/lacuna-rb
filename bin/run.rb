9daf0938ea97489c9504fae36cb0cd74ec46add1
Vasari <nmccallum7@gmail.com>
2014-06-30 13:15:13 +1000

    Allow multiple tasks in one run.

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,23 @@
 # encoding: utf-8
 
-load_path = File.join(
-    File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'lacuna_util.rb')
-
+here = File.expand_path(File.dirname(__FILE__))
+load_path = File.join(here, '..', 'lib', 'lacuna_util.rb')
 require load_path
-task = LacunaUtil.task(ARGV[0]).new
-task.run
+
+# ARGV should only be a list of tasks, eg: ruby run.rb CleanMail UpgradeBuildings
+tasks = ARGV
+to_run = []
+
+# Preemptively check each task the user specifies and try to report errors.
+tasks.each do |name|
+    # LacunaUtil will stop the script if the task doesn't exist
+    task = LacunaUtil.task(name)
+
+    # If we didn't get any errors, push it into the array that will get run through later.
+    to_run << task
+end
+
+
+to_run.each do |lets_go|
+    lets_go.new.run
+end
